d96953f4bdf2526c01e734b115873c1e554bd08c
Edouard Murat <edouardmurat1@gmail.com>
2022-08-02 09:48:35 -0400

    First commit: CSG root node is converted sucessfully to MeshInstance, but surfaces don't follow yet

---
+++ addons/csgtomeshinstance/plugin.gd
@@ -0,0 +1,36 @@
+# Plugin to PBR paint a selected MeshInstance through the use of a custom PBR shader
+# Main script that creates and destroys the plugin 
+
+tool
+extends EditorPlugin
+
+var plugin_button :PluginButton
+
+func selection_changed() -> void:
+	var selection = get_editor_interface().get_selection().get_selected_nodes()
+	
+	var can_convert = selection.size() == 1 and selection[0] is CSGShape and selection[0].is_root_shape()
+	
+	# If selected object in tree is csg
+	if can_convert:
+		var root = get_tree().get_edited_scene_root()
+		plugin_button.show_button(root, selection[0])
+	else:
+		plugin_button.hide_button()
+
+# Create whole plugin
+func _enter_tree():
+	# Add button to 3D scene UI
+	# Shows panel when toggled
+	plugin_button = preload("res://addons/csgtomeshinstance/plugin_button.tscn").instance()
+	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, plugin_button)
+	plugin_button.hide()
+	
+	# Spy on event when object selected in tree changes
+	get_editor_interface().get_selection().connect("selection_changed", self, "selection_changed")
+
+# Destroy whole plugin
+func _exit_tree():
+	remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, plugin_button)
+	if plugin_button:
+		plugin_button.free()
---
+++ addons/csgtomeshinstance/plugin_button.gd
@@ -0,0 +1,30 @@
+# Button to activate the painting and show the paint panel
+
+tool
+extends Button
+
+class_name PluginButton
+
+var root :Node
+var csg :CSGShape
+
+# Show button in UI, untoggled
+func show_button(root: Node, csg :CSGShape):
+	self.root = root
+	self.csg = csg
+	show()
+
+# Hide button in UI, untoggled
+func hide_button():
+	hide()
+
+func _on_PluginButton_pressed() -> void:
+	convert_csg_to_meshinstance()
+
+func convert_csg_to_meshinstance():
+	var mesh_instance = MeshInstance.new()
+	var csg_mesh = csg.get_meshes()[1]
+	mesh_instance.mesh = csg_mesh
+	csg.get_parent().add_child(mesh_instance)
+	mesh_instance.owner = root
+	csg.queue_free()
